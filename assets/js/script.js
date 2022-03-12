12d5de3c19b1591a0c0efe2afbc904300236a430
lnd4812 <laureldavid64@gmail.com>
2022-03-11 21:10:21 -0500

    added window objects to obtain password component inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,59 @@
 // Assignment code here
 
 // user clicks on "generate password button"
-document.getElementById("").addEventListener("click", function(){
+document.getElementById("generate").addEventListener("click", function(){
 
 });
 
-// ask user if they'd like to generate a new password
-window.alert("Welcome to your Password Generator LND2022.  Please answer the following questions in order to generate your new secure password");
+  // ask user to answer a series of questions to provide the necessary components of the password
+  window.confirm("Welcome to your Password Generator LND2022.  Please click on the button and answer the following questions in order to create your new password");
 
-// ask user how many characters they want their password to comprise
-window.prompt("Please select a number from 8 to 128 to set the length of your new password");
-
-// when user enters the number, that number is recorded somewhere 
-
-
-// ask user if they'd like to include capital letters in the new password
-window.confirm("Would you like your password to include Capital letters?");
-  //If yes - include command to include, else all small letters - validate input and select at least one letter
+  // proceed through following steps
   
-// ask user if they'd like to include any numbers in the new password
-window.confirm("Would you like your password to include numbers?");
-// if yes, function to include numbers, else function to exclude - in either case, validate innput & select at least one number
+  // (click) ask user how many characters they want their password to comprise
+  var passwordLength = window.prompt("Please select a number from 8 to 128 to set the length of your new password");
 
+  
+    // (click) ask user if they'd like to include capital letters in the new password
+    var includeUpperCase = window.confirm("Would you like your password to include Capital letters?");
+      //If yes - include command to include, else all small letters - validate input and select at least one letter
 
-// ask user if they'd like to include any special characters in the new password
-window.confirm("Would you like your password to include special characters?");
+      if (includeUpperCase) {
+        // if yes, function to include at least one Upper Case letter, else function to exclude - in either case, validate input & select at least one number
+      
+      } 
+      else {
+        // ensure any letters added to the password are lower case only
+      }
+ 
+       
+  // (click) ask user if they'd like to include any numbers in the new password
 
-// if yes, include special characters, else do not - in either case, validate input and select at least one special character
+    var includeNumber = window.confirm("Would you like your password to include numbers?");
+  
+    if (includeNumber) {
+      // if yes, function to include numbers, else function to exclude - in either case, validate input & select at least one number
+      //some function
+    } 
 
+    else {
+      // ensure no numbers are included in password
+    }
+ 
+  // (click) ask user if they'd like to include any special characters in the new password
+  var includeSpecial = window.confirm ("Would you like your password to include special characters?");
 
-// ask user to click the button again to generate their new password
-window.alert("Thank you for confirming your choices.  You may now click the button to generate your new password.");
+    // if yes, include special characters, else do not - in either case, validate input and select at least one special character
+    if(includeSpecial) {
+      // ensure at least one special character is included within password
+    } 
+    else {
+      // ensure no special characters are included in password
 
-// add event listener to run function that generates new and random password
+  
+  
 
-// display new password
-window.alert("Your Secure Password " + password );
+  } 
 
 
 
@@ -56,54 +74,6 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+// ask user to click the button again to generate their new password
+window.alert("Thank you for confirming your choices.  You may now click the button to generate your new password.");
 
-
-//document.getElementById("button1").addEventListener("click", function(){
-
-  //document.getElementById("box").style.height = "300px";
-  //document.getElementById("box").style.width = "300px";
-
-//});
-
-//document.getElementById("button2").addEventListener("click", function(){
-
-  //document.getElementById("box").style.backgroundColor = "blue";
-
-///});
-
-//document.getElementById("button3").addEventListener("click", function(){
-
-  //document.getElementById("box").style.opacity = "25%";
-//});
-
-
-//document.getElementById("button4").addEventListener("click", function(){
-
-  //document.getElementById("box").style.borderRadius = "100%";
-//});
-
-
-//document.getElementById("button5").addEventListener("click", function(){
-
-  //document.getElementById("box").style.borderStyle= "solid";
-  //document.getElementById("box").style.borderWidth = "1px%";
-  //document.getElementById("box").style.borderColor = "black";
-//});
-
-//document.getElementById("button6").addEventListener("click", function(){
-
-     // document.getElementById("box").style.marginLeft = "100px";
-//});
-
-
-//document.getElementById("button7").addEventListener("click", function(){
-
-  //document.getElementById("box").style.height = "150px";
-  //document.getElementById("box").style.width = "150px";
-  //document.getElementById("box").style.backgroundColor = "orange";
-  //document.getElementById("box").style.opacity = "1";
-  //document.getElementById("box").style.borderRadius = "0%";
-  //document.getElementById("box").style.borderStyle = "none";
-  //document.getElementById("box").style.margin = "25px";
-
-//})//
