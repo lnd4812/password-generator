1a6edc8c29e21f939ce36d9a9d9ff441eb3cc09e
lnd4812 <laureldavid64@gmail.com>
2022-03-12 21:28:09 -0500

    password printing properly; input for it still requires editing

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,26 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var writePassword = function() {//
+var writePassword = function() {
   // ask user to answer a series of questions to provide the necessary components of the password; if user hits cancel, program does not proceed
   var proceed = window.confirm("Welcome to the Password Generator LND2022.  If you would like to create a new password, please click OK, otherwise please click on the CANCEL button");
   
   // if user pressed OK, proceed through following steps
-  if(proceed) 
-    // ask user how many characters they want in their password
-    var newPasswordLength = window.prompt("Please select a number from 8 to 128 to set the length of your new password");
-
-    // ask user if they'd like to include capital letters in the new password
-    var includeUpperCase = window.confirm("Would you like your password to include Capital letters?");
-      //If yes - include command to include, else all small letters - validate input and select at least one letter
-      if(includeUpperCase) {
-        // if yes, function to include at least one Upper Case letter, else function to exclude
-        var letterInput = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
-        } 
-        else {
-        // ensure any letters added to the password are lower case only
-        } var letterInput = "abcdefghijklmnopqrstuvwxyz"; 
-        
-      // ask user if they'd like to include any numbers in the new password
+  if(proceed) {
+    // Letter components 
+      // ask user if they'd like to include capital letters in the new password
+      var includeUpperCase = window.confirm("Would you like your password to include Capital letters?");
+        //If yes - include command to include, else all small letters - validate input and select at least one letter
+        if(includeUpperCase) {
+          // if yes, function to include at least one Upper Case letter, else function to exclude
+          var letterInput = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
+          } 
+          else {
+          // ensure any letters added to the password are lower case only
+          } var letterInput = "abcdefghijklmnopqrstuvwxyz"; 
+      
+    //Numeric Components    
+      // ask user if they'd like to include any numberic values in the new password
       var includeNum = window.confirm("Would you like your password to include numbers?");
         // If yes, create var with numbers 
         if(includeNum) {
@@ -32,45 +31,42 @@ var writePassword = function() {//
         } 
         else {
           // ensure number string is empty
-        } var numInput = "!#$%()*+,-./:;<=>?@[\]^_`{|}~";
+        } var numInput = "";
       
+    // Special Character Components 
       // ask user if they'd like to include any special characters in the new password
       var specialCharacters = window.confirm("Would you like your password to include special characters?");
         // if yes, include special characters, otherwise no
         if(specialCharacters) {
         // ensure at least one special character is included within password
-        var specialCharInput = "";
+        var specialCharInput = "!#$%()*+,-./:;<=>?@[\]^_`{|}~";
         } 
         else {
         // ensure no special characters are included in password
         var specialCharInput = ""; // specialCharInput has no value
         }
-      
-    //create an array to include selected options from above steps
+    
+    //combine the user's selections into a single string
     var passwordCombo = (letterInput + numInput + specialCharInput);
+    
+    //check
     console.log(passwordCombo);
-    console.log(newPasswordLength);
-    
-    // Write password to the #password input
-    var generatePassword = function(length) {
-    //continue adding characters from array until number required are strung together to form password 
-        var password = "";
-        for(var i = 0; i < length + 1; i++ ) {
-          password = passwordCombo.charAt(Math.floor(Math.random() * passwordCombo.length));
-        }
-        return password;
         
+    // Write password to the #password input
+   var newPasswordLength = window.prompt("Please select a number from 8 to 128 to set the length of your new password");  //continue adding characters from array until number required are strung together to form password   
+   function generatePassword() {   
+   // ask user how many characters they want in their password
+   var password = '';
+      for(var i = 0; i < newPasswordLength; i++ ) {
+        password += passwordCombo.charAt(Math.floor(Math.random() * passwordCombo.length));
       }
-     generatePassword(newPasswordLength); 
-
-     var password = generatePassword();
-     var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-    
-    };     
-
+      var passwordText = document.querySelector("#password");
+      passwordText.value = password;
+    }
+    generatePassword(newPasswordLength);      
+  }
+};
 writePassword();  
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
