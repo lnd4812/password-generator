38b3f3812350cae781acf4248788d568b66a05bd
lnd4812 <laureldavid64@gmail.com>
2022-03-12 20:36:13 -0500

    create README.md draft and additional coding for testing

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Password Generator Starter Code
+# Password Generator Challenge
+
+The purpose of this project is to use Javascript functions along with concepts learned in Module 3 to generate a password using a modal created from and styled within the starter code.  An event handler has also been included in the starter code as a means of initiating the password generation function from a button embedded in the modal.
+
+The user is asked to choose the number of characters to be included in the password, ranging from 8 to 128 and is then asked a series of questions to determine whether the new password will include some combination of Upper and lower case letters, numbers and/or special characters.  A "for" loop in the function will iterate through a concatenation of the selected input variables until the desired length has been satisfied and output the result to a text field in the modal.
+
+## Requirements
+
+No special requirements beyond meeting the acceptance criteria of the chanllenge.
+
+## Built With
+
+HTML
+CSS
+Javascript
+
+## Deployment
+
+This site has been published via [Github Pages](https://lnd4812.github.io/password-generator/).
+The repository has been stored on [Github](https://github.com/lnd4812/password-generator.git).
+
+## Design
+
+The main design uses the modal created from the Starter Code that enables the user to go go through a series of steps that will determine the length and content of the new password. This is accomplished using a series of window object commands such as window.prompt for the user to indicate the number of characters in the new password and a series of window.confirm commands to determine the component types of the password.
+
+flush out: click event to generate button to begin password function
+questions to complete password criteria
+formula to compile the password
+outputting the password...
+
+## License
+
+An MIT [License] has been included with this project.
+
+## Credits and external Resources
+
+A full description of Challenge 3's criteria is outlined here: [Module 3 Challenge Project Page](https://courses.bootcampspot.com/courses/1181/assignments/23403?module_item_id=459947).
+Additional assistance was provided by the TA's during Office Hours.
+
+## Task list
+
+[ ]
+
+## Contribution
+
+Laurel David (github.com/lnd4812)
+
+## Contact Information
+
+For any questions, comments and/or issues, please contact me at email: laureldavid64@gmail.com
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,69 +3,73 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var writePassword = function() {//The click event
+var writePassword = function() {//
   // ask user to answer a series of questions to provide the necessary components of the password; if user hits cancel, program does not proceed
-  var proceed = window.confirm("Welcome to your Password Generator LND2022.  If you would like to create a new password, please click OK, otherwise please click on the CANCEL button");
+  var proceed = window.confirm("Welcome to the Password Generator LND2022.  If you would like to create a new password, please click OK, otherwise please click on the CANCEL button");
   
-  // if used pressed OK, proceed through following steps
+  // if user pressed OK, proceed through following steps
   if(proceed) 
-
     // ask user how many characters they want in their password
     var newPasswordLength = window.prompt("Please select a number from 8 to 128 to set the length of your new password");
-      
-    console.log(newPasswordLength);
+
     // ask user if they'd like to include capital letters in the new password
-      var includeUpperCase = window.confirm("Would you like your password to include Capital letters?");
-        //If yes - include command to include, else all small letters - validate input and select at least one letter
-        if(includeUpperCase) {
-        // if yes, function to include at least one Upper Case letter, else function to exclude - in either case, validate input & select at least one number
-        var letterInput = ["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"];  
+    var includeUpperCase = window.confirm("Would you like your password to include Capital letters?");
+      //If yes - include command to include, else all small letters - validate input and select at least one letter
+      if(includeUpperCase) {
+        // if yes, function to include at least one Upper Case letter, else function to exclude
+        var letterInput = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
         } 
         else {
         // ensure any letters added to the password are lower case only
-        } var letterInput = ["abcdefghijklmnopqrstuvwxyz"];  
+        } var letterInput = "abcdefghijklmnopqrstuvwxyz"; 
         
       // ask user if they'd like to include any numbers in the new password
-      var includeNumber = window.confirm("Would you like your password to include numbers?");
-        // If yes, create var with numbers
-        if(includeNumber) {
-          // if yes, function to include at least one Upper Case letter, else function to exclude - in either case, validate input & select at least one number
-          var numInput = ["0123456789"];  
-          } 
-          else {
-          // ensure any letters added to the password are lower case only
-          } var numInput = "";  
-
+      var includeNum = window.confirm("Would you like your password to include numbers?");
+        // If yes, create var with numbers 
+        if(includeNum) {
+        // if yes, function to include at least one Upper Case letter, else function to exclude - in either case, validate input & select at least one number
+        var numInput = "0123456789";
+        } 
+        else {
+          // ensure number string is empty
+        } var numInput = "!#$%()*+,-./:;<=>?@[\]^_`{|}~";
+      
       // ask user if they'd like to include any special characters in the new password
-      var includeSpecial = window.confirm("Would you like your password to include special characters?");
-        // if yes, include special characters, else do not - in either case, validate input and select at least one special character
-        if(includeSpecial) {
+      var specialCharacters = window.confirm("Would you like your password to include special characters?");
+        // if yes, include special characters, otherwise no
+        if(specialCharacters) {
         // ensure at least one special character is included within password
-        var specialCharInput = ["~!@#$%^&*"];
+        var specialCharInput = "";
         } 
         else {
         // ensure no special characters are included in password
         var specialCharInput = ""; // specialCharInput has no value
-        } 
+        }
       
     //create an array to include selected options from above steps
     var passwordCombo = (letterInput + numInput + specialCharInput);
     console.log(passwordCombo);
+    console.log(newPasswordLength);
     
     // Write password to the #password input
-       
-      //concatenate values from array until number of required characters are strung together to form password 
+    var generatePassword = function(length) {
+    //continue adding characters from array until number required are strung together to form password 
         var password = "";
-        for(var i = 0; i < newPasswordLength + 1; i++ ) {
-          var generatePassword = Math.floor(Math.random() * newPasswordLength);
-          password = passwordCombo[generatePassword];
+        for(var i = 0; i < length + 1; i++ ) {
+          password = passwordCombo.charAt(Math.floor(Math.random() * passwordCombo.length));
         }
         return password;
-}
-        var passwordText = document.querySelector("#password");
-        passwordText.value = password;
+        
+      }
+     generatePassword(newPasswordLength); 
 
- writePassword();  
+     var password = generatePassword();
+     var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+    
+    };     
+
+writePassword();  
 
 
 // Add event listener to generate button
