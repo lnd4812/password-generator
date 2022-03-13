77d5cb117851754f7727cb2a5a54f52d2ba19128
lnd4812 <laureldavid64@gmail.com>
2022-03-13 12:47:32 -0400

    updated wording to discontinue process if user chooses not to proceed or enters number outside of acceptable range

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,27 @@
 var generateBtn = document.querySelector("#generate");
 
 var writePassword = function() {
-  
-  
-  document.getElementById("#password").addEventListener("click", function(){
-  document.getElementById("password") = null});
+    
+  //document.getElementById("#password").addEventListener("click", function(){
+  //document.getElementById("password") = null});
     
   // ask user to answer a series of questions to provide the necessary components of the password; if user hits cancel, program does not proceed
   var proceed = window.confirm("Welcome to the Password Generator LND2022.  If you would like to create a new password, please click OK, otherwise please click on the CANCEL button");
   
   // if user pressed OK, proceed through following steps
-  if(proceed) {
+  if(proceed === false) {
+    window.alert("Thank you. Goodbye");
+    return;
+  }   else  {
+      //start process
       var newPasswordLength = window.prompt("Please select a number from 8 to 128 to set the length of your new password");
       // confirm number input is within required range
-      if(console.log(newPasswordLength) < 8 || console.log(newPasswordLength) > 128) {
-      window.alert("You picked an incorrect option.  Please try again, entering a number between 8 and 128.");
-      //return to window prompt command)
-      writePassword();
-      } 
-      else {
-    
-      // have user enter preferences based on the following 3 components
+      if(newPasswordLength < 8 || newPasswordLength > 128) {
+        window.alert("You have entered an invalid number.  Please restart, choosing a number between 8 and 128.");
+        return;
+      }
+      else { // User to now enter preferences based on the following 3 components
+      
       // Letter components 
       // ask user if they'd like to include capital letters in the new password
       var includeUpperCase = window.confirm("Would you like your password to include Capital letters?");
